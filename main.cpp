0191f329318a650643265001d0ec24fdb4a02912
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-09-14 20:42:34 -0400

    added 2 source files andtsv file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 // add your code as appropriate 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include "reservoir.h"
 
 int main()
 {
-  
+  get_east_storage("05/20/2018");
 
-  
   return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -1,1 +1,37 @@
 // add your code
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include "reservoir.h"
+
+double get_east_storage(std::string date) {
+  
+  std::string junk;
+  std::string Point_time;
+  double eastSt;
+  double eastEl;
+  double westSt;
+  double westEl;
+  
+  std::ifstream fin("Current_Reservoir_Levels.tsv");
+  
+  if (fin.fail()) {
+    std::cerr << "File cannot be opened for reading." << std::endl;
+    exit(1); // exit if failed to open the file
+  }
+  
+  getline(fin, junk); // read one line from the file
+
+  while (Point_time.compare(date) != 0){
+    fin >> Point_time;
+  }
+
+  fin >> eastSt;
+  
+  std::cout << date << " " << "East basin storage: " << eastSt << " billion gallons \n";
+  
+  fin.close();
+  
+  return 0;
+}
